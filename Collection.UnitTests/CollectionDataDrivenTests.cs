410124d79947bda3a4dd72dbdd6362a509cee196
Maria Emanuilova <mariyakoseva@yahoo.com>
2023-01-27 21:05:58 +0200

    Added data driven test cases for Collection

---
+++ Collection.UnitTests/CollectionDataDrivenTests.cs
@@ -0,0 +1,22 @@
+﻿using Collections;
+using System.Globalization;
+
+namespace Collection.UnitTests
+{
+    public class CollectionDataDrivenTests
+    {
+       [TestCase("Peter,Maria,Ivan",0,"Peter")]
+       [TestCase("Peter,Maria,Ivan", 1, "Maria")]
+       [TestCase("Peter,Maria,Ivan", 2, "Ivan")]
+       [TestCase("Peter", 0, "Peter")]
+
+        public void Test_Collection_GetByValidIndex(string data, int index, string expected)
+        {
+            var coll = new Collection<string>(data.Split(","));
+            var actual = coll[index];
+
+            Assert.That(actual, Is.EqualTo(expected));    
+        }
+
+    }
+}
